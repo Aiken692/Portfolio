1bfdabb2942e0fa0016ca14997ccb12549832c27
Walter Ruganzu <rwalter692@gmail.com>
2021-04-21 18:00:53 +0300

    2nd commit

--- script.js
+++ script.js
@@ -5,12 +5,37 @@ $(document).ready(function(){
         }else{
             $('.navbar').removeClass("sticky")
         }
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show")
+        }else{
+            $('.scroll-up-btn').removeClass("show")
+        }
     });
+
+    // Slide up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0})
+    })
 
     // toggle menu/navbar script
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
+    });
+
+    // Animation for typing
+    var typed = new Typed(".typing", {
+        strings: ["Developer", "Designer", "Accountant", "FreeLancer"],
+        typedSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["Developer", "Designer", "Accountant", "FreeLancer"],
+        typedSpeed: 100,
+        backSpeed: 60,
+        loop: true
     });
 
     // owl carousel script
